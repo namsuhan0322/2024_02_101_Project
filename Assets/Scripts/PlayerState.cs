28a2178afc9bce54434d98a4bafbde48a23615af
namsuhan0322 <rnatpakfl@gmail.com>
2024-10-23 10:55:53 +0900

    [Feat] PlayerAnimationManager 작성, 카메라 수정

--- Assets/Scripts/PlayerState.cs
+++ Assets/Scripts/PlayerState.cs
@@ -3,38 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // PlayerState 모든 플레이어 상태의 기본이 되는 추상 클래스
-public abstract class PlayerState : MonoBehaviour
+public abstract class PlayerState
 {
-    protected PlayerStateMachine stateMachine;        // 상태 머신에 대한 참조 (이후 구현)
+    protected PlayerStateMachine stateMachine;          // 상태 머신에 대한 참조 (이후 구현)
     protected PlayerController playerController;        // 플레이어 컨트롤러에 대한 참조
+    protected PlayerAnimationManager animationManager;  // 애니메이션 매니저를 가져온다.
 
     // 생성자 : 상태 머신과 플레이어 컨트롤러 참조 초기화
     public PlayerState(PlayerStateMachine stateMachine)
     {
         this.stateMachine = stateMachine;
         this.playerController = stateMachine.PlayerController;
+        this.animationManager = stateMachine.GetComponent<PlayerAnimationManager>();
     }
 
     // 가상 메서드들 : 하위 클래스에서 필요에 따라 오버라이드
-    public virtual void Enter()             // 상태 진입 시 호출
-    {
-
-    }
-
-    public virtual void Exit()              // 상태 종료 시 호출
-    {
-
-    }   
-
-    public virtual void Update()            // 매 프레임 호출
-    {
-
-    }
-
-    public virtual void FixedUpdate()     // 고정 시간 간격으로 호출 (물리 연산용)
-    {
-
-    }
+    public virtual void Enter() { }             // 상태 진입 시 호출
+    public virtual void Exit() { }              // 상태 종료 시 호출  
+    public virtual void Update() { }            // 매 프레임 호출
+    public virtual void FixedUpdate() { }       // 고정 시간 간격으로 호출 (물리 연산용)
 
     // 상태 전호나 조건을 체크하는 메서드
     protected void CheckTransitions()
@@ -73,10 +60,7 @@ public abstract class PlayerState : MonoBehaviour
 // IdleState : 플레이어가 정지해 있는 상태
 public class IdleState : PlayerState
 {
-    public IdleState(PlayerStateMachine stateMachine) : base(stateMachine)
-    {
-
-    }
+    public IdleState(PlayerStateMachine stateMachine) : base(stateMachine) { }
 
     public override void Update()
     {
@@ -87,13 +71,14 @@ public class IdleState : PlayerState
 // MoveState : 플레이어가 움직이고 있는 상태
 public class MovingState : PlayerState
 {
-    public MovingState(PlayerStateMachine stateMachine) : base(stateMachine)
-    {
-
-    }
+    private bool isRunnig;
+    public MovingState(PlayerStateMachine stateMachine) : base(stateMachine) { }
 
     public override void Update()
     {
+        // 달리기 입력 확인
+        isRunnig = Input.GetKey(KeyCode.LeftShift);
+
         CheckTransitions();                 // 매 프레임마다 상태 전환 조건 체크
     }
 
@@ -106,10 +91,7 @@ public class MovingState : PlayerState
 // JumpingState : 플레이어가 점프하는 상태
 public class JumpingState : PlayerState
 {
-    public JumpingState(PlayerStateMachine stateMachine) : base(stateMachine)
-    {
-
-    }
+    public JumpingState(PlayerStateMachine stateMachine) : base(stateMachine) { }
 
     public override void Update()
     {
@@ -125,10 +107,7 @@ public class JumpingState : PlayerState
 // FallingState : 플레이어가 낙하 중일때
 public class FallingState : PlayerState
 {
-    public FallingState(PlayerStateMachine stateMachine) : base(stateMachine)
-    {
-
-    }
+    public FallingState(PlayerStateMachine stateMachine) : base(stateMachine) { }
 
     public override void Update()
     {
